eab4be42c8b39bdcc3988b0358df4e9a85ac8ee7
christospap10 <xristospapotis@gmail.com>
2021-10-14 20:21:30 +0300

    Final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,3 +40,47 @@ function scrollHeader() {
   }
 }
 window.addEventListener("scroll", scrollHeader);
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY > 200) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SCROLL TO REVEAL ===============*/
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  //reset: true,
+});
+
+sr.reveal(".home__header, .section__title, .specs", { delay: 400 });
+sr.reveal(".home__footer", { delay: 700 });
+sr.reveal(".home__img", { delay: 900, origin: "left" });
